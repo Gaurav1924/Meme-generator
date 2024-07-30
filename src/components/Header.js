45f1f0438f9dc581f1159ff96a5eb5988b8b5851
unknown <gauravkshatriya99@gmail.com>
2024-07-30 15:38:00 +0530

    files restructed and dependencies installed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,16 @@
 import React from "react"
-
+import "../style.css" ;
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.png" 
+                src="../images/troll-face.png" 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">
+                Developed by Gaurav
+            </h4>
         </header>
     )
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,5 @@
 import React from "react"
-
+import "../style.css" ;
 export default function Meme() {
     const [meme, setMeme] = React.useState({
         topText: "",
